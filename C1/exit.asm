68c51873efb12cc78b461473743b709dc77831e2
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2026-03-09 10:59:02 +0800

    update .gitignore

--- C1/exit.asm
+++ C1/exit.asm
@@ -1,1 +1,18 @@
-; Program: exit
+;   Program: exit
+;
+;   Executes the exit system call
+;
+;   No input
+;
+;   Output: only the exit status
+;           $? in the shell
+;
+        segment .text
+        global  start
+start:
+        mov     eax, 60     ; 60 is the exit syscall number
+        mov     edi, 5      ; the status value to return
+        syscall             ; execute a system call
+
+        end
+
